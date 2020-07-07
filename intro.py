30cbec9ca6611ec1cba0fbb7ef87c3c5d0e6cf9e
Combatd <combatdstar@gmail.com>
2020-07-07 05:47:32 -0700

    Define fred(), jane()

--- intro.py
+++ intro.py
@@ -1,6 +1,16 @@
-temp = "5 degrees"
-cel = 0
-try:
-    fahr = float(temp)
-    cel = (fahr - 32.0) * 5.0 / 9.0
-    print(cel)
+# temp = "5 degrees"
+# cel = 0
+# try:
+#     fahr = float(temp)
+#     cel = (fahr - 32.0) * 5.0 / 9.0
+#     print(cel)
+
+def fred():
+    print("Zap")
+
+def jane():
+    print("ABC")
+
+jane()
+fred()
+jane()
